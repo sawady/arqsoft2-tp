64423101cd8ac06f2649bf889e3ee6840af813f1
Federico Sawady <sawady.faso@gmail.com>
2016-03-20 00:12:44 -0300

    mongoDB

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,28 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import scala.concurrent.Future
+import javax.inject.Inject
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import repositories.FooRepository
 
-class Application extends Controller {
+class Application @Inject() (val fooRepository: FooRepository)
+    extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def create(name: String, age: Int) = Action.async {
+    fooRepository.save(name, age).map(msg =>
+      Ok(msg))
+  }
+
+  def getAllFoos = Action.async {
+    fooRepository.find().map { items =>
+      Ok(items)
+    }
   }
 
 }
