a9f14384c45cedcc0c53f90cde2671fea917694e
Sawady <sawady.faso@gmail.com>
2016-06-11 19:48:06 -0300

    nueva config para visibilidad de todos los nodos

--- run-server-docker.sh
+++ run-server-docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+export mongo1=172.17.0.2
+export mongo2=172.17.0.3
+export mongo3=172.17.0.4
+
 docker run -v /home/vm/Escritorio/arqsoft2-tp/target/build-dev:/opt/app \
 -v /home/vm/Escritorio/arqsoft2-tp/newrelic:/opt/newrelic -p 9000:9000 \
---name play-server --link play-mongo:mongo -h server.play \
+--name play-server \
+-h server.play.com \
+--add-host mongo1.play.com:${mongo1} \
+--add-host mongo2.play.com:${mongo2} \
+--add-host mongo3.play.com:${mongo3} \
 --rm beevelop/java:latest /opt/app/bin/arqsof2-tp \
 -J-javaagent:/opt/newrelic/newrelic.jar -Dnewrelic.config.file=./opt/newrelic/newrelic.yml \
 -Dconfig.resource=application-docker.conf
